f2b047a480f97a1afe6aa42e4c59025c007762da
phact <estevezsebastian@gmail.com>
2017-09-05 16:39:34 -0400

    setup graph

--- schema.groovy
+++ schema.groovy
@@ -1,4 +1,10 @@
-schema.config().option('graph.schema_mode').set('Development')
+:remote close
+:remote connect tinkerpop.server conf/remote.yaml session-managed
+:remote config timeout max
+:remote console
+system.graph('s').ifNotExists().create()
+
+:remote config alias g s.g
 
 schema.propertyKey("account_number").Text().single().create()
 schema.propertyKey("trade_country").Text().single().create()
@@ -149,3 +155,39 @@ Fund53.addEdge ('holds', HoldingF53_D)
 Fund53.addEdge ('holds', HoldingF53_F)
 Fund53.addEdge ('holds', HoldingF53_J)
 Fund53.addEdge ('holds', HoldingF53_L)
+:remote config alias g s.g
+
+Fund73.addEdge ('holds', HoldingF73_A)
+Fund73.addEdge ('holds', HoldingF73_C)
+Fund73.addEdge ('holds', HoldingF73_E)
+Fund73.addEdge ('holds', HoldingF73_H)
+Fund73.addEdge ('holds', HoldingF73_K)
+Fund73.addEdge ('holds', HoldingF73_L)
+
+HoldingF73_A.addEdge ('asset', cusipA)
+HoldingF73_C.addEdge ('asset', cusipC)
+HoldingF73_E.addEdge ('asset', cusipE)
+HoldingF73_H.addEdge ('asset', cusipH)
+HoldingF73_K.addEdge ('asset', cusipK)
+HoldingF73_L.addEdge ('asset', cusipL)
+
+alexAccount.addEdge ('owns', Fund51)
+alexAccount.addEdge ('owns', Fund52)
+alexAccount.addEdge ('owns', Fund73)
+
+
+
+HoldingF53_A.addEdge ('asset', cusipA)
+HoldingF53_B.addEdge ('asset', cusipB)
+HoldingF53_D.addEdge ('asset', cusipD)
+HoldingF53_F.addEdge ('asset', cusipF)
+HoldingF53_G.addEdge ('asset', cusipG)
+HoldingF53_L.addEdge ('asset', cusipL)
+
+Fund53.addEdge ('holds', HoldingF53_A)
+Fund53.addEdge ('holds', HoldingF53_B)
+Fund53.addEdge ('holds', HoldingF53_D)
+Fund53.addEdge ('holds', HoldingF53_F)
+Fund53.addEdge ('holds', HoldingF53_G)
+Fund53.addEdge ('holds', HoldingF53_L)
+
